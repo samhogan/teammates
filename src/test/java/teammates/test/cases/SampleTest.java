f6cd73aab9bf7cf2f39f5fba8f40c91858e2a600
Sam Hogan <sam@samhogan.net>
2019-02-02 19:01:45 -0500

    actually added unit test

--- src/test/java/teammates/test/cases/SampleTest.java
+++ src/test/java/teammates/test/cases/SampleTest.java
@@ -1,4 +1,17 @@
 package teammates.test.cases;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+import teammates.common.util.StringHelper;
+
 public class SampleTest {
+
+
+    @Test
+    public void givenLongString_whenTruncate_thenReturnTruncatedWithEllipsis()
+    {
+        String longString = "This is a very long string that needs to be truncated";
+        String truncatedString = StringHelper.truncate(longString, 10);
+        assertEquals(truncatedString, "This is..."); //uses .equals() internally
+    }
 }
